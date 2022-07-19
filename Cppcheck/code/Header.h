b55b34e4020c1c631bc4b0090cfda74ec9780357
AlexaRost <54909428+AlexaRost@users.noreply.github.com>
2022-07-19 17:13:01 +0300

    Add files via upload

---
+++ Cppcheck/code/Header.h
@@ -0,0 +1,25 @@
+#pragma once
+int t() {
+	char a[10];
+	a[11] = 0;
+	return 0;
+}
+void f() {
+	int ct;
+	while (ct < 100)
+	{
+		std::cout << ct;
+	}
+}
+void g() {
+	for (int i; i < 1; i++) {
+		i++;
+	}
+}
+double Division(int n, int k) {
+
+	double res = 0;
+	return n/k;
+}
+
+
