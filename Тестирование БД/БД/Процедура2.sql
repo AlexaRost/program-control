9fff2c9fb4cb9d675ef11bac271d7f20a0d4e90d
AlexaRost <54909428+AlexaRost@users.noreply.github.com>
2022-07-19 16:43:52 +0300

    Update Процедура2.sql

--- Тестирование БД/БД/Процедура2.sql
+++ Тестирование БД/БД/Процедура2.sql
@@ -24,4 +24,4 @@ SELECT  SurCl, Category, NumCl, Cty FROM Client
 JOIN Contract ON Contract.Id_Cl = Client.Id_Cl
 JOIN Serv ON Serv.Id_Serv = Contract.Id_Serv
 WHERE Typ = @Tp ORDER BY SurCl;
-END
+END
